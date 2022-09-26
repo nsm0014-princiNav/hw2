03dcbc943263314ea2f43c2b3a166a7024086395
Noah Miller <nsm0014@auburn.edu>
2022-09-26 12:44:01 -0500

    printing

--- Q9.m
+++ Q9.m
@@ -4,9 +4,11 @@ clear
 close all
 format shortg 
 %% Begin Question 9
-
+% C_gamma_alpha
 DCM = genDCM('deg',180, 3)
-
-DCM = genDCM('deg',[45 180], [1 3])
-
+% C_gamma_beta
+DCM1 = genDCM('deg',[45 180], [1 3])
+% C_alpha_beta
 DCM = genDCM('deg',[45], [1])
+
+DCM1*[-1;3;0]
