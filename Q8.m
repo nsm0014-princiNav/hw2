a9ee980640431e3cc611a4695743b2e81184511c
Noah Miller <nsm0014@auburn.edu>
2022-09-27 01:03:17 -0500

    Corrected and Ready to Print

--- Q8.m
+++ Q8.m
@@ -5,8 +5,8 @@ close all
 format shortg
 
 %% Begin Question 8
-DCM = genDCM('deg',[120 45 -120],[1 2 3])
+DCM = genDCM('deg',[120 45 -120],[3 2 1])
 
 phi = atand(-DCM(2,3)/DCM(3,3))
-theta = atand(DCM(1,3)/(sqrt(DCM(2,3)^2 + DCM(3,3)^2)))
+theta = atand(-DCM(1,3)/(sqrt(DCM(2,3)^2 + DCM(3,3)^2)))
 psi = atand(-DCM(1,2)/DCM(1,1))
